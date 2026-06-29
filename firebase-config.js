f8b370d23a0b0e7db52144667a8903dc464b147a
asg2030 <asag12@gmail.com>
2026-06-29 07:06:57 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDCFwPAcHtyb_VlMzg3ZyiRO8b_GIQNyEY",
+  apiKey: "AIzaSyD1PBHewjNOe0JlijkmcGU9gAzJu1J3_IQ",
   authDomain: "amoria-admission.firebaseapp.com",
   databaseURL: "https://amoria-admission-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "amoria-admission",
   storageBucket: "amoria-admission.firebasestorage.app",
   messagingSenderId: "506992451228",
-  appId: "1:506992451228:web:9651b5db19bb8864c09759",
-  measurementId: "G-N0X0V9T56L"
+  appId: "1:506992451228:web:db118ac12e35a1e6c09759",
+  measurementId: "G-QVTJ36Q6RP"
 };
+
+firebase.initializeApp(firebaseConfig);
